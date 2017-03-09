3d2888804f06dab393ae3445e8d07f198859d3ff
mina <mina.samaan@student.guc.edu.eg>
2017-03-09 22:45:14 +0200

    first

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express()
+//---------------------------------------------------------------------------------------------------------------------
+var crypto = require('crypto');
+var ecdh = crypto.createECDH('secp256k1');
+ecdh.generateKeys();
+console.log(ecdh.getPublicKey());
+var publicKey = ecdh.getPublicKey(null,'compressed');
+var privateKey = ecdh.getPrivateKey(null, 'compressed');
+var clientPublicKey;
+var sharedKey;
+//ENCRYPTION-----------------------------------------------------------------------------------------------------------
+var aesjs = require('aes-js');
+var aesCtr;
+//---------------------------------------------------------------------------------------------------------------------
+app.get('/publicKey', function(req, res) {
+  res.json(publicKey)
+})
+app.get('/sharedKey', function(req, res) {
+  res.json(publicKey)
+})
+ 
+app.listen(3000)
