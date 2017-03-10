e63747b411d346bff6ab9b0c2ed760c84542824a
mina <mina.samaan@student.guc.edu.eg>
2017-03-10 11:29:08 +0200

    4th

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ var app = express()
 //---------------------------------------------------------------------------------------------------------------------
 var crypto = require('crypto');
 var ecdh = crypto.createECDH('secp256k1');
-ecdh.generateKeys();
-console.log(ecdh.getPublicKey());
-var publicKey = ecdh.getPublicKey(null,'compressed');
-var privateKey = ecdh.getPrivateKey(null, 'compressed');
 var clientPublicKey;
 var sharedKey;
 //ENCRYPTION-----------------------------------------------------------------------------------------------------------
@@ -14,13 +10,18 @@ var aesjs = require('aes-js');
 var aesCtr;
 //---------------------------------------------------------------------------------------------------------------------
 app.get('/', function(req, res) {
-  res.json("publicKey")
+  res.send("I am Mina's Api ECDH Server")
 })
+
 app.get('/publicKey', function(req, res) {
-  res.json(publicKey)
+ecdh.generateKeys();
+var publicKey = ecdh.getPublicKey(null,'compressed');
+var privateKey = ecdh.getPrivateKey(null, 'compressed');
+  res.send(publicKey)
 })
-app.get('/sharedKey', function(req, res) {
-  res.json(publicKey)
+
+app.get('/sharedKey/:public', function(req, res) {   ecdh.computeSecret(JSON.parse(req.params.public));
 })
  
 app.listen(process.env.PORT ||8080, ()=>console.log("ok"))
+
