b03bd18521acd372f9b5783dbe7d659cc1d1083f
mina <mina.samaan@student.guc.edu.eg>
2017-04-29 10:53:33 +0200

    nonce added

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-var express = require('express')
-var app = express()
-//---------------------------------------------------------------------------------------------------------------------
+//API_Server--------------------------------------------------------------------------------------------------
+var express = require('express');
+var app = express();
+//ECDH--------------------------------------------------------------------------------------------------------
 var crypto = require('crypto');
 var ecdh;
 var clientPublicKey;
 var sharedKey;
 var publicKey;
 var privateKey;
-//ENCRYPTION-----------------------------------------------------------------------------------------------------------
+//AES_Encryption----------------------------------------------------------------------------------------------
 var aesjs = require('aes-js');
 var aesCtr;
-//---------------------------------------------------------------------------------------------------------------------
+//RSA_Encryption----------------------------------------------------------------------------------------------
 var cryptico = require('cryptico');
-//--------------------------------------------------------------------------------------------------------------
+//Salt_&_Nonce------------------------------------------------------------------------------------------------
 var genRandomString = function(length){
     return crypto.randomBytes(Math.ceil(length/2))
             .toString('hex') /** convert to hexadecimal format */
             .slice(0,length);   /** return required number of characters */
 };
-
+//SHA512------------------------------------------------------------------------------------------------------
 var sha512 = function(password, salt){
     var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
     hash.update(password);
@@ -27,25 +28,25 @@ var sha512 = function(password, salt){
     return {
         salt:salt,
         passwordHash:value
-
-}};
-//---------------------------------------------------------------------------------------------------------------------
+           }
+};
+//Routes-----------------------------------------------------------------------------------------------------
 app.get('/', function(req, res) {
   res.write("Hello, am Mina Samir's Security Api Server");
   res.end();
 })
-//key_exchange_and_AES-------------------------------------------------------------------------------------------------------------
+
 app.get('/register/:password', function(req, res) {
 	ecdh= crypto.createECDH('secp256k1');
 	ecdh.generateKeys();
-  AesPublicKey = ecdh.getPublicKey(null,'compressed');
-  AesPrivateKey = ecdh.getPrivateKey(null, 'compressed');
-  var salt =genRandomString(16);
+  var AesPublicKey = ecdh.getPublicKey(null,'compressed');
+  var AesPrivateKey = ecdh.getPrivateKey(null, 'compressed');
+  var salt = genRandomString(16);
   var passwordData = sha512(req.params.password, salt);
   var RsaPrivate = cryptico.generateRSAKey(req.params.password, 1024);
   var RsaPublic = cryptico.publicKeyString(rsaKeys);
   res.json({
-    salt:salt,
+    salt: salt,
     hash: passwordData.passwordHash,
     AesPublicKey: AesPublicKey,
     AesPrivateKey: AesPrivateKey,
@@ -57,25 +58,74 @@ app.get('/register/:password', function(req, res) {
 app.get('/sharedKey/:severPublic/:userPrivate', function(req, res) {
   var serverkey = new Buffer.from(JSON.parse(req.params.serverPublic));
   ecdh.setPrivateKey(new Buffer.from(JSON.parse(req.params.userPrivate)));
-
   sharedKey=ecdh.computeSecret(serverkey);
-	//aesCtr = new aesjs.ModeOfOperation.ctr(sharedKey);
-    res.send(JSON.stringify(sharedKey));
+  res.send(JSON.stringify(sharedKey));
 })
 
 app.get('/publicKey', function(req, res) {
 	ecdh= crypto.createECDH('secp256k1');
 	ecdh.generateKeys();
-publicKey = ecdh.getPublicKey(null,'compressed');
-privateKey = ecdh.getPrivateKey(null, 'compressed');
-
+  publicKey = ecdh.getPublicKey(null,'compressed');
+  privateKey = ecdh.getPrivateKey(null, 'compressed');
   res.send(JSON.stringify(publicKey));
 })
 
 app.get('/privateKey', function(req, res) {
-
   res.send(JSON.stringify(privateKey));
 })
+
+//        ****for encryption and decryption you should use set sharedkey route first****
+
+app.get('/setSharedKey/:shared', function(req, res) {
+  var buf = new Buffer.from(JSON.parse(req.params.shared));
+  aesCtr = new aesjs.ModeOfOperation.ctr(buf);
+  res.send("Shared key Ready");
+})
+
+app.get('/decrypt/:bytes', function(req, res) {
+  var buf=JSON.parse(req.params.bytes);
+  var arr = [];
+  for(var p in Object.getOwnPropertyNames(buf)) {
+  arr[p] = buf[p];}
+  console.log('the sharedKey before encryption ::: ' +sharedKey.toString('hex'));
+  console.log('these are the encryptedBytes ::: ' +req.params.bytes);
+  var decryptedBytes = aesCtr.decrypt(arr);
+  console.log('these are the decryptedBytes ::: ' +decryptedBytes);
+  var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+  res.send(decryptedText);
+})
+
+app.get('/encrypt/:text', function(req, res) {
+var textBytes = aesjs.utils.utf8.toBytes(req.params.text);
+var encryptedBytes = aesCtr.encrypt(textBytes);
+console.log('the sharedKey before encryption ::: ' +sharedKey.toString('hex'));
+console.log('these are the textBytes ::: ' +textBytes);
+console.log('these are the encryptedBytes ::: ' +encryptedBytes);
+res.send(JSON.stringify(encryptedBytes));
+})
+
+//authentication----------------------------------------------------------------------------------------------
+
+app.get('/salt', function(req, res) {
+  res.send(genRandomString(16));
+})
+
+app.get('/nonce', function(req, res) {
+  res.send(genRandomString(32));
+})
+
+app.get('/time', function(req, res) {
+  var timeStamp = Date.now();
+  res.json(timeStamp);
+})
+
+app.get('/passwordHash/:salt/:password', function(req, res) {
+  var passwordData = sha512(req.params.password, req.params.salt);
+  res.send(passwordData.passwordHash);
+})
+
+app.listen(process.env.PORT ||8080, ()=>console.log("ok"))
+
 
 // app.get('/sharedKey/:public', function(req, res) {
 // var buf = new Buffer.from(JSON.parse(req.params.public));
@@ -85,53 +135,3 @@ app.get('/privateKey', function(req, res) {
 // 	//aesCtr = new aesjs.ModeOfOperation.ctr(sharedKey);
 //     res.send(JSON.stringify(sharedKey));
 // })
-app.get('/setSharedKey/:shared', function(req, res) {
-var buf = new Buffer.from(JSON.parse(req.params.shared));
-	aesCtr = new aesjs.ModeOfOperation.ctr(buf);
-    res.send("Shared key Ready");
-})
-//  ****for encryption and decryption you should use set sharedkey route first****
-app.get('/decrypt/:bytes', function(req, res) {
-var buf=JSON.parse(req.params.bytes);
-	var arr = [];
-		for(var p in Object.getOwnPropertyNames(buf)) {
-		    arr[p] = buf[p];
-		}
-    console.log('the sharedKey before encryption ::: ' +sharedKey.toString('hex'));
-    console.log('these are the encryptedBytes ::: ' +req.params.bytes);
-
-var decryptedBytes = aesCtr.decrypt(arr);
-    console.log('these are the decryptedBytes ::: ' +decryptedBytes);
-var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
-res.send(decryptedText);
-
-})
-app.get('/encrypt/:text', function(req, res) {
-var textBytes = aesjs.utils.utf8.toBytes(req.params.text);
-var encryptedBytes = aesCtr.encrypt(textBytes);
-console.log('the sharedKey before encryption ::: ' +sharedKey.toString('hex'));
-console.log('these are the textBytes ::: ' +textBytes);
-console.log('these are the encryptedBytes ::: ' +encryptedBytes);
-res.send(JSON.stringify(encryptedBytes));
-
-})
-//authentication-----------------------------------------------------------------------------------------------------------------
-
-app.get('/salt', function(req, res) {
-
-  res.send(genRandomString(16));
-})
-
-app.get('/time', function(req, res) {
-var timeStamp = Date.now();
-res.json(timeStamp);
-
-})
-
-app.get('/passwordHash/:salt/:password', function(req, res) {
-var passwordData = sha512(req.params.password, req.params.salt);
-res.send(passwordData.passwordHash);
-})
-
-
-app.listen(process.env.PORT ||8080, ()=>console.log("ok"))
